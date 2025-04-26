a76dda1e8d2634d90addcea67231fb2f9fd4aee2
Nir Eyal <nireyal8@gmail.com>
2025-04-26 09:51:17 +0300

    feat:more logging to page loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { App } from './App';
+import '@/styles/globals.css';
 
 console.log('Starting application...');
 console.log('Base URL:', import.meta.env.BASE_URL);
